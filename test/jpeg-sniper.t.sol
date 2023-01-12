59d0fde35581912487b8b959fea4c8fcf67313ad
Poor4ever <0xpoor4ever@proton.me>
2023-01-12 19:51:06 +0800

    upload jpeg sniper challenge

---
+++ test/jpeg-sniper.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/jpeg-sniper/FlatLaunchpeg.sol";
+import "../src/jpeg-sniper/Exploit.sol";
+
+contract jpegSniper is Test {
+    uint internal blockNumber;
+    uint256 internal constant COLLECTION_SIZE = 69;
+    uint256 internal constant MAX_BATCH_SIZE = 5;
+    uint256 internal constant MAX_PER_ADDRESS_DURING_MINT = 5;
+
+    address internal attacker;
+    FlatLaunchpeg internal flatlaunchpeg;
+    Exploit internal exploit;
+
+    function setUp() public {
+        attacker = address(bytes20("attacker"));
+        vm.label(attacker, "Attacker");
+
+        flatlaunchpeg = new FlatLaunchpeg(COLLECTION_SIZE, MAX_BATCH_SIZE, MAX_PER_ADDRESS_DURING_MINT);
+        vm.label(address(flatlaunchpeg), "FlatLaunchpeg");
+
+        blockNumber = block.number;
+    }
+
+    function testExploit() public {
+        vm.startPrank(attacker);
+        //Deploy your exploit contract and complete the challenge!
+        exploit = new Exploit();
+        vm.stopPrank();
+        verify();
+    }
+
+    function verify() internal {
+        assertEq(flatlaunchpeg.balanceOf(attacker), 69);
+        assertEq(flatlaunchpeg.totalSupply(), 69);
+        assertEq(block.number,  blockNumber + 1);
+    }
+}
