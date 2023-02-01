d0b11138053c4763344615a352c667eeb234e444
Poor4ever <0xpoor4ever@proton.me>
2023-02-01 11:59:43 +0800

    Modify some errors

--- test/free-lunch.t.sol
+++ test/free-lunch.t.sol
@@ -131,8 +131,8 @@ contract freeLunch is Test {
         *******************************************/
         
         // attacker has increased both SAFU and USDC funds by at least 50x
-        assertGt(usdc.balanceOf(attacker), ATTACKER_INIT_TOKEN_AMOUNT * 50);
-        assertGt(safu.balanceOf(attacker), ATTACKER_INIT_TOKEN_AMOUNT * 50);
+        assertGe(usdc.balanceOf(attacker), ATTACKER_INIT_TOKEN_AMOUNT * 50);
+        assertGe(safu.balanceOf(attacker), ATTACKER_INIT_TOKEN_AMOUNT * 50);
     }
 
 }
